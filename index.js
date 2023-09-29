6d7b753f812537d26f588c1ebca1cb367fd35c5c
Seth Davis <sethdavis512@gmail.com>
2023-09-29 09:03:16 -0500

    Adjust templates

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
 #!/usr/bin/env node
-import path from 'node:path';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import minimist from 'minimist';
 import { Plop, run } from 'plop';
 
 const args = process.argv.slice(2);
 const argv = minimist(args);
-
-import { dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
-
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const config = {
-    cwd: argv.cwd,
-    configPath: path.join(__dirname, 'plopfile.js'),
-    preload: argv.preload || [],
-    completion: argv.completion
-};
-
-const callback = (env) => Plop.execute(env, run);
-
-Plop.prepare(config, callback);
+Plop.prepare(
+    {
+        cwd: argv.cwd,
+        configPath: join(__dirname, 'plopfile.js'),
+        preload: argv.preload || [],
+        completion: argv.completion
+    },
+    (env) => Plop.execute(env, run)
+);
