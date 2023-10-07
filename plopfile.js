7a48889def696cfe6300ba033c6858f9e0616a66
Seth Davis <sethdavis512@gmail.com>
2023-10-06 21:06:30 -0500

    Add tiny react generator and templates

--- plopfile.js
+++ plopfile.js
@@ -1,19 +1,19 @@
-export default function (plop) {
-    plop.setGenerator('ts-component', {
-        description: 'A React component and unit test written in Typescript',
+export default function ({ setGenerator }) {
+    setGenerator('tiny-react', {
+        description: 'Tiny React app built using Parcel',
         prompts: [
             {
                 type: 'input',
                 name: 'name',
-                message: 'Component name'
+                message: 'Name'
             }
         ],
         actions: [
             {
                 type: 'addMany',
-                destination: `${process.cwd()}/{{ pascalCase name }}`,
-                templateFiles: 'plop-templates/ts-component',
-                base: 'plop-templates/ts-component'
+                destination: `${process.cwd()}/{{ kebabCase name }}/`,
+                templateFiles: `plop-templates/tiny-react/**/*.hbs`,
+                base: `plop-templates/tiny-react`
             }
         ]
     });
