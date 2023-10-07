c51397d8f76ca1a8acab32e75bb60688b0364410
Seth Davis <sethdavis512@gmail.com>
2023-10-06 23:09:59 -0500

    Add async csv generator and template

--- plopfile.js
+++ plopfile.js
@@ -1,19 +1,57 @@
+import fs from 'fs';
+import os from 'os';
+
+const fetchTodos = async () => {
+    return fetch('https://jsonplaceholder.typicode.com/todos').then(
+        (response) => response.json()
+    );
+};
+
+const read = async (filePath) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) reject(err);
+            resolve(data);
+        });
+    });
+};
+
 export default function ({ setGenerator }) {
-    setGenerator('hello-world', {
-        description: 'First generator',
+    setGenerator('async', {
+        description: 'Async generator with custom action',
         prompts: [
             {
                 type: 'input',
                 name: 'name',
-                message: 'Your name'
+                message: 'File name'
             }
         ],
         actions: [
-            {
-                type: 'addMany',
-                destination: process.cwd(),
-                templateFiles: 'plop-templates/hello-world',
-                base: 'plop-templates/hello-world'
+            async function customAction(answers, config, plop) {
+                try {
+                    const todos = await fetchTodos();
+                    const templatePath = `${os.homedir()}/repositories/jarvis/plop-templates/async/table.csv.hbs`;
+                    const rawData = {
+                        data: todos
+                    };
+
+                    const compiled = plop.renderString(
+                        await read(templatePath),
+                        rawData
+                    );
+
+                    await fs.writeFile(
+                        `${answers.name}.csv`,
+                        compiled,
+                        (err) => {
+                            if (err) console.error(err);
+                        }
+                    );
+
+                    return 'Successfully wrote CSV file';
+                } catch (error) {
+                    console.log(error);
+                }
             }
         ]
     });
